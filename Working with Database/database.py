539f8a1109f050ceccec568c5ff050a0b7994b4d
HungTruong4689 <hungphuc4689@gmail.com>
2024-03-31 12:53:38 +0300

    multiple records

--- Working with Database/database.py
+++ Working with Database/database.py
@@ -41,3 +41,29 @@ cur.mogrify(query2,data)
 cur.execute(query2,data)
 ##Commit to make it permanent
 conn.commit()
+
+###Insert mutilple records
+## executemany
+import psycopg2 as db
+from faker import Faker
+fake = Faker()
+data = []
+i = 2
+for r in range(1000):
+    data.append((i,fake.name(),fake.street_address(), fake.city(), fake.zipcode()))
+    i+=1
+
+data_for_db = tuple(data)
+
+conn_string = "dbname= 'dataengineering' host='localhost' user='postgres' password='postgres'"
+
+#Create the connection object by passing the connection string to the connect() method
+conn = db.connect(conn_string)
+cur = conn.cursor()
+
+query = "insert to users(id,name,street,city,zip) value(%s,%s,%s,%s,%s)"
+
+print(cur.mogrify(query,data_for_db[1]))
+
+cur.executemany(query,data_for_db)
+conn.commit()
